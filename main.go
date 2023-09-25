e39ef9847821d8879a9feb16a0c2756f43240e2f
gschussler <grantschussler@gmail.com>
2023-09-25 17:29:00 -0500

    transfer localdir to repo

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+)
+
+func main() {
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, World!")
+	})
+
+	// start http server
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+}
